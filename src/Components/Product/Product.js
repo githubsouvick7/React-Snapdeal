63c51273b3b2c854cec7f1e0e173baa29b3643a1
Souvick Chakraborty <hellosouvickk@gmail.com>
2023-02-14 18:38:54 +0530

    commit two

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -7,6 +7,8 @@ const api = `https://content.newtonschool.co/v1/pr/63b6c911af4f30335b4b3b89/prod
 
 const Product = () => {
     const [eData, seteData] = useState([]);
+    let white = '#fff'
+    const [bgColor, setBgColor] = useState(white);
 
     const getData = () => {
         fetch(api)
@@ -17,6 +19,11 @@ const Product = () => {
         getData();
     }, [])
 
+    const colorChange = () => {
+        let red = 'rgb(163, 13, 13)'
+        setBgColor(red);
+    }
+
     return (
         <>
             <div className="main">
@@ -25,10 +32,13 @@ const Product = () => {
                         eData.map(item => (
                             <>
                                 <div className='compcard'>
-                                    <img width={200} src={item.image} />
+                                    <img width={150} src={item.image} />
                                     <div className="desc">
+                                        <hr />
                                         <h4>{item.title}</h4>
                                         <p>{item.price}</p>
+                                        <button className="btn">Add to Cart</button>
+                                        <span onClick={colorChange} className='wish'><i className='fa-sharp fa-regular fa-heart'></i></span>
                                     </div>
                                 </div>
                             </>
